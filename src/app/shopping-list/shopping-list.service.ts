8396d2687fbdc72826d67d03c553e393d6b83e5d
akashkshekarappa <33307773+akashkshekarappa@users.noreply.github.com>
2020-03-25 00:36:46 -0400

    Routing

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,8 @@
-import {Ingredient} from '../shared/ingredient.model';
-import {EventEmitter} from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient>();
-
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -19,12 +18,10 @@ export class ShoppingListService {
   }
 
   addIngredients(ingredients: Ingredient[]) {
-    /*for(let ingredient of ingredients) {
-      this.addIngredient(ingredient);
-    }*/
+    // for (let ingredient of ingredients) {
+    //   this.addIngredient(ingredient);
+    // }
     this.ingredients.push(...ingredients);
-    this.ingredients.emit(this.ingredients.slice());
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
 }
-
-
